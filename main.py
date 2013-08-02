40b5d529d4053d65523f4e0d711e029c222c7f88
Simon Lenz <dev@simon-lenz.de>
2013-08-02 18:56:13 +0200

    added cropping of the image and saveing as png

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-img_cropped = "data/radical_cropped.png"
-img_cropped_gauss = "data/radical_cropped.png"
+path_screenshots_log = "data/unedited/log"
+path_screenshots_pop = "data/unedited/pop"
+path_screenshots_pop_a_log = "data/unedited/pop_a_log"
 
-imgfile = img_cropped_gauss
+path_to_test_image = "data/unedited/log/HoI3_6.bmp"
+
+from PIL import Image
+from os.path import splitext
+
+
+def prepare_image(imagepath):
+    image = Image.open(imagepath)
+    cropped_path = splitext(imagepath)[0] + ".png"
+
+    image.crop((866, 914, 866 + 439, 914 + 137)).save(cropped_path)
+
 
 def ex1(path):
     import cv2.cv as cv
     import tesseract
 
-    image=cv.LoadImage(path, cv.CV_LOAD_IMAGE_GRAYSCALE)
+    image = cv.LoadImage(path, cv.CV_LOAD_IMAGE_GRAYSCALE)
 
     api = tesseract.TessBaseAPI()
-    api.Init(".","eng",tesseract.OEM_DEFAULT)
+    api.Init(".", "eng", tesseract.OEM_DEFAULT)
     #api.SetPageSegMode(tesseract.PSM_SINGLE_WORD)
     api.SetPageSegMode(tesseract.PSM_AUTO)
-    tesseract.SetCvImage(image,api)
-    text=api.GetUTF8Text()
-    conf=api.MeanTextConf()
+    tesseract.SetCvImage(image, api)
+    text = api.GetUTF8Text()
+    conf = api.MeanTextConf()
     print "Confidence of following: {}".format(conf)
     print text
 
 print "Start"
-ex1(img_cropped)
-ex1(img_cropped_gauss)
+prepare_image(path_to_test_image)
 print "End"
