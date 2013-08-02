c92fd5b9c909b229efb56460bce1999f5f759358
Simon Lenz <dev@simon-lenz.de>
2013-08-02 20:24:16 +0200

    added gauss filter and fixed call calls

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ path_screenshots_log = "data/unedited/log"
 path_screenshots_pop = "data/unedited/pop"
 path_screenshots_pop_a_log = "data/unedited/pop_a_log"
 
-path_to_test_image = "data/unedited/log/HoI3_6.bmp"
-
 from PIL import Image, ImageOps
+from os import remove
 from os.path import splitext
+from subprocess import check_call
+from scipy.misc import imread, imsave
+from scipy.ndimage.filters import gaussian_filter
 
 
 def prepare_image(imagepath):
     image = Image.open(imagepath)
-    cropped_path = splitext(imagepath)[0] + ".png"
+    prepared_path = splitext(imagepath)[0] + ".png"
 
     # crop the gamelog out
     image = image.crop((866, 914, 866 + 439, 914 + 137))
@@ -20,9 +22,17 @@ def prepare_image(imagepath):
     image = image.convert("L")  # to black and white
     threshold = 40
     image = image.point(lambda p: p > threshold and 255)
+    #########################
+    image.save(prepared_path)
+    #########################
+    #gauss
+    image = imread(prepared_path)
+    image = gaussian_filter(image, sigma=0.55)
+    imsave(prepared_path, image)
 
-    image.save(cropped_path)
+    image = Image.open(prepared_path)
     image.show()
+    return prepared_path
 
 
 def ex1(path):
@@ -41,6 +51,17 @@ def ex1(path):
     print "Confidence of following: {}".format(conf)
     print text
 
-print "Start"
-prepare_image(path_to_test_image)
+print "Start-test suite"
+path_to_test_image = "./data/unedited/log/HoI3_6.bmp"
+path_to_prepared_test_image = prepare_image(path_to_test_image)
+tmp_outbase_path = "./tmp_tesseract_file"
+print "image to process: ", path_to_prepared_test_image
+print "temporary path to result text: ", tmp_outbase_path
+check_call(["tesseract", path_to_prepared_test_image, tmp_outbase_path])
+
+tes_output_file = open(tmp_outbase_path + ".txt")
+ocr_output = tes_output_file.read()
+print ocr_output
+
+remove(tmp_outbase_path + ".txt")
 print "End"
