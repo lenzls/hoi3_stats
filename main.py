acea9b05eedc16e9875bed402cc96da714db335a
Simon Lenz <dev@simon-lenz.de>
2013-08-02 23:39:42 +0200

    refactor, reorga

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ path_screenshots_pop_a_log = "data/unedited/pop_a_log"
 
 tmp_outbase_path = "./tmp_tesseract_file"
 
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageFilter
 import Levenshtein
-from os import remove
-from os.path import splitext
+from os import remove, listdir
+from os.path import splitext, join
 from random import random
 from subprocess import check_call
 from scipy.misc import imread, imsave
@@ -29,16 +29,24 @@ def prepare_image(imagepath, sigma, img_scale_factor):
     image = image.point(lambda p: p > threshold and 255)
     #resize
     w, h = image.size
-    image = image.resize((int(w * img_scale_factor), int(h * img_scale_factor)), Image.NEAREST)
+    image = image.resize((int(w * img_scale_factor), int(h * img_scale_factor)), Image.ANTIALIAS)
+
     #########################
     image.save(prepared_path)
     #########################
     #gauss
     image = imread(prepared_path)
     image = gaussian_filter(image, sigma=sigma)
+    #########################
     imsave(prepared_path, image)
-
+    #########################
     image = Image.open(prepared_path)
+    image = image.filter(ImageFilter.UnsharpMask)
+    #########################
+    image.save(prepared_path)
+    #########################
+
+    #image = Image.open(prepared_path)
     #image.show()
     return prepared_path
 
@@ -58,36 +66,60 @@ def check_for_best_sigma(imagepath):
             print "sig: ", sigma
             print "img_scale_factor: ", img_scale_factor
             path_to_prepared_test_image = prepare_image(imagepath, sigma, img_scale_factor)
-            check_call(["tesseract", path_to_prepared_test_image, tmp_outbase_path])
+            check_call(["tesseract", path_to_prepared_test_image, tmp_outbase_path, "quiet"])
             goodness = evaluate_image(tmp_outbase_path + ".txt", test_image_solution)
             print "distance between guess and solution (more = better): ", goodness
             if goodness > best_args[0]:
                 best_args = goodness, test_image_solution, sigma, img_scale_factor
     return best_args
 
-print "Start-test suite"
-path_to_test_image = "./data/unedited/log/HoI3_6.bmp"
-test_image_solution = """14:00, 19 October, 1941 133a Divisione 'Littorio' arrived in Balashov
-16:00, 19 October1 1941 Hostile planes are performing a Ground Attack in
-Tandaho.
-316:00, 19 October1 1941 We won the Batle of Korenovsk. We lost 1084 of
-46967, Soviet Union lost 380 of 8998
 
-17:00, 19 October1 1941 1a Divisione Alpina 'Taurinese' arrived in
+def check_example_file():
+    print "Start-test suite"
+    path_to_test_image = "./data/unedited/log/HoI3_6.bmp"
+    test_image_solution = """14:00, 19 October, 1941 133a Divisione 'Littorio' arrived in Balashov
+    16:00, 19 October1 1941 Hostile planes are performing a Ground Attack in
+    Tandaho.
+    316:00, 19 October1 1941 We won the Batle of Korenovsk. We lost 1084 of
+    46967, Soviet Union lost 380 of 8998
 
-Korenovsk
-"""
+    17:00, 19 October1 1941 1a Divisione Alpina 'Taurinese' arrived in
 
-b_goodness, b_solution, b_sigma, b_img_scale_factor = check_for_best_sigma(path_to_test_image)
+    Korenovsk
+    """
 
-tmp_path = prepare_image(path_to_test_image, b_sigma, b_img_scale_factor)
-check_call(["tesseract", tmp_path, tmp_outbase_path])
-goodness = evaluate_image(tmp_outbase_path + ".txt", test_image_solution)
-print "Best arguments ({}%): sigma = {} img_scale_factor = {}".format(b_goodness, b_sigma, b_img_scale_factor)
-print "Guess:\n{}".format(b_solution)
-image = Image.open(tmp_path)
-image.show()
+    b_goodness, b_solution, b_sigma, b_img_scale_factor = check_for_best_sigma(path_to_test_image)
+
+    tmp_path = prepare_image(path_to_test_image, b_sigma, b_img_scale_factor)
+    check_call(["tesseract", tmp_path, tmp_outbase_path, "quiet"])
+    goodness = evaluate_image(tmp_outbase_path + ".txt", test_image_solution)
+    print "Best arguments ({}%): sigma = {} img_scale_factor = {}".format(b_goodness, b_sigma, b_img_scale_factor)
+    print "Guess:\n{}".format(b_solution)
+    image = Image.open(tmp_path)
+    image.show()
 
 
-remove(tmp_outbase_path + ".txt")
-print "End"
+def guess_all_log():
+    # experimentally determined
+    sigma = 0.8
+    img_scale_factor = 2.779
+
+    for image_name in listdir(path_screenshots_log):
+        if not image_name.endswith(".bmp"):
+            continue
+        image_path = join(path_screenshots_log, image_name)
+        print image_path
+        tmp_path = prepare_image(image_path, sigma, img_scale_factor)
+        check_call(["tesseract", tmp_path, tmp_outbase_path, "quiet"])
+        tes_output_file = open(tmp_outbase_path + ".txt")
+        ocr_output = tes_output_file.read()
+        tes_output_file.close()
+        print "Guess:\n{}".format(ocr_output)
+
+
+def cleanup():
+    remove(tmp_outbase_path + ".txt")
+
+guess_all_log()
+
+cleanup()
