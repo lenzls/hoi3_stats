9f24a93ffdd49437bf8d68a7609b7b029f7c51a7
Simon Lenz <dev@simon-lenz.de>
2013-08-02 21:14:07 +0200

    added check for best sigma

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ path_screenshots_log = "data/unedited/log"
 path_screenshots_pop = "data/unedited/pop"
 path_screenshots_pop_a_log = "data/unedited/pop_a_log"
 
+tmp_outbase_path = "./tmp_tesseract_file"
+
 from PIL import Image, ImageOps
 import Levenshtein
 from os import remove
@@ -9,9 +11,10 @@ from os.path import splitext
 from subprocess import check_call
 from scipy.misc import imread, imsave
 from scipy.ndimage.filters import gaussian_filter
+from numpy import linspace
 
 
-def prepare_image(imagepath):
+def prepare_image(imagepath, sigma):
     image = Image.open(imagepath)
     prepared_path = splitext(imagepath)[0] + ".png"
 
@@ -28,35 +31,43 @@ def prepare_image(imagepath):
     #########################
     #gauss
     image = imread(prepared_path)
-    image = gaussian_filter(image, sigma=0.55)
+    image = gaussian_filter(image, sigma=sigma)
     imsave(prepared_path, image)
 
     image = Image.open(prepared_path)
-    image.show()
+    #image.show()
     return prepared_path
+
+
+def evaluate_image(imagepath, solution):
+    tes_output_file = open(imagepath)
+    ocr_output = tes_output_file.read()
+    tes_output_file.close()
+    #print ocr_output
+    print "distance between guess and solution (fewer = better): ", Levenshtein.ratio(ocr_output, solution)
+
+
+def check_for_best_sigma(imagepath):
+    for sigma in linspace(0., 1., 10):
+        print "sig: ", sigma
+        path_to_prepared_test_image = prepare_image(imagepath, sigma)
+        check_call(["tesseract", path_to_prepared_test_image, tmp_outbase_path])
+        evaluate_image(tmp_outbase_path + ".txt", test_image_solution)
 
 print "Start-test suite"
 path_to_test_image = "./data/unedited/log/HoI3_6.bmp"
 test_image_solution = """14:00, 19 October, 1941 133a Divisione 'Littorio' arrived in Balashov
 16:00, 19 October1 1941 Hostile planes are performing a Ground Attack in
 Tandaho.
-16:00, 19 October1 1941 We won the Batle of Korenovsk. We lost 1084 of
+316:00, 19 October1 1941 We won the Batle of Korenovsk. We lost 1084 of
 46967, Soviet Union lost 380 of 8998
+
 17:00, 19 October1 1941 1a Divisione Alpina 'Taurinese' arrived in
+
 Korenovsk
 """
 
-path_to_prepared_test_image = prepare_image(path_to_test_image)
-tmp_outbase_path = "./tmp_tesseract_file"
-print "image to process: ", path_to_prepared_test_image
-print "temporary path to result text: ", tmp_outbase_path
-check_call(["tesseract", path_to_prepared_test_image, tmp_outbase_path])
-
-tes_output_file = open(tmp_outbase_path + ".txt")
-ocr_output = tes_output_file.read()
-print ocr_output
-
-print Levenshtein.ratio(ocr_output, test_image_solution)
+check_for_best_sigma(path_to_test_image)
 
 remove(tmp_outbase_path + ".txt")
 print "End"
