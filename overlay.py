8df37cbe3875475b2ab53ff509ff5fc6442d2417
Simon Lenz <lenz.simon@googlemail.com>
2013-08-04 00:36:06 +0200

    initial version of the overlay with hotkey

--- overlay.py
+++ overlay.py
@@ -1,20 +1,35 @@
 import Tkinter as Tk
+import lib.pyhk
+from PIL import ImageGrab 
+
+
+hot = lib.pyhk.pyhk()
 
 class Overlay:
 	def __init__(self, master):
-		frame = Tk.Frame(master)
-		frame.pack()
+		self.master = master
+		self.frame = Tk.Frame(master)
+		self.frame.pack()
 
-		self.but1 = Tk.Button(frame, text="Quit", command=frame.quit)
-		self.but1.pack(side=Tk.LEFT)
+		self.screenshot_button = Tk.Button(self.frame, text="Screenshot (Ctrl + Alt + s)", command=self.makeScreenshot)
+		self.screenshot_button.pack(side=Tk.LEFT)
 
-		self.but2 = Tk.Button(frame, text="Hey", command=self.printThis)
-		self.but2.pack(side=Tk.LEFT)
+		self.quit_button = Tk.Button(self.frame, text="Quit", command=self.frame.quit)
+		self.quit_button.pack(side=Tk.LEFT)
 
-	def printThis(self):
-		print "hello"
+
+		screenshot_shortcut = hot.addHotkey(['Ctrl','Alt','S'], self.makeScreenshot)
+
+	def makeScreenshot(self):
+		screenshot_path = "data/screenshot.png"
+		print "Generate Screenshot at {}".format(screenshot_path)
+		ImageGrab.grab().save(screenshot_path)
 
 root_widget = Tk.Tk()
+root_widget.attributes("-topmost", 1)
+root_widget.title("HoI3_statistics overlay")
+root_widget.iconbitmap(default='icon.ico')
+
 
 overlay = Overlay(root_widget)
 
