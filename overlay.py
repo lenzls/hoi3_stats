729673e90a38ed8fe96217499ed185a7c1d0396a
Simon Lenz <lenz.simon@googlemail.com>
2013-08-04 12:10:53 +0200

    added correction text field

--- overlay.py
+++ overlay.py
@@ -20,14 +20,25 @@ class Overlay():
 		self.status_label = Tk.Label(self.frame, textvariable=self.status_label_text)
 		self.status_label.pack(side=Tk.BOTTOM)
 
+		self.correction_text = Tk.Text(self.frame)
+		self.correction_text.pack(side=Tk.BOTTOM)
+		self.correction_text.insert(1.0, "This is the correction text field")
+
 		self.screenshot_button = Tk.Button(self.frame, text="Screenshot (Ctrl + Alt + s)", command=logger.makeScreenshot)
 		self.screenshot_button.pack(side=Tk.LEFT)
+
+		self.continue_button = Tk.Button(self.frame, text="Continue", command=self.set_correction_text)
+		self.continue_button.pack(side=Tk.LEFT)
 
 		self.quit_button = Tk.Button(self.frame, text="Quit", command=self.frame.quit)
 		self.quit_button.pack(side=Tk.LEFT)
 
 		screenshot_shortcut = hotkey.addHotkey(['Ctrl','Alt','S'], logger.makeScreenshot)
 
+	def set_correction_text(self, multiline_String="test"):
+		self.correction_text.delete(1.0, Tk.END)
+		self.correction_text.insert(1.0, multiline_String)
+		
 	def set_status_text(self, status_string):
 		print "setting overlay label"
 		self.status_label_text.set(status_string)
